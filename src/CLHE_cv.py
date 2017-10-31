61a05b16e1dc01cc3639ad54c6d45824017fc6c4
crackcodecamp <justrock.akki@gmail.com>
2017-10-31 08:20:49 +0530

    After Revaluation 1

--- src/CLHE_cv.py
+++ src/CLHE_cv.py
@@ -1,11 +1,12 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('wiki1.png',0)
+img = cv2.imread('img/inp_13.jpg',0)
 
 # create a CLAHE object (Arguments are optional).
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 cl1 = clahe.apply(img)
 
-cv2.imwrite('clahe_2.jpg',cl1)
+cv2.imwrite('img/out_c_13.jpg',cl1)
+
 
